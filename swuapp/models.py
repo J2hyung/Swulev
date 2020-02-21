182a7a10b611ffa0ec317a041ba4f24e42198727
SimbaJiHyeon <jhk8806@likelion.org>
2020-02-22 00:53:35 +0900

    user_lecture DB 생성

--- swuapp/models.py
+++ swuapp/models.py
@@ -4,4 +4,14 @@ from django.db import models
 class User(models.Model):
     userid = models.CharField(max_length = 20)
     userpw = models.CharField(max_length = 30)
-    
+
+class UserLecture(models.Model):
+
+    RATING_FIELD = (
+        ('on', 'on'),
+        ('off', 'off'),
+    )
+
+    lectureid = models.ForeignKey('User', on_delete=models.CASCADE, related_name='lectureid', primary_key=True)
+    semester = models.CharField(max_length = 30)
+    rating = models.CharField(max_length=10, choices=RATING_FIELD, default = "off")
