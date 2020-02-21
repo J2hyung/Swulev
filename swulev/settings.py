452cae2236c9e270d136e235c7c8e82f025600c7
Your Name <you@example.com>
2020-02-22 01:45:27 +0900

    settings수정

--- swulev/settings.py
+++ swulev/settings.py
@@ -69,20 +69,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'swulev',
-<<<<<<< HEAD
         'USER' : 'swulev',
         'PASSWORD' : 'likelion1234',
         'HOST' : 'swulev.chkailloujbx.ap-northeast-2.rds.amazonaws.com',
         'PORT' : '3306',
-=======
-        'USER': 'swulev',
-        'PASSWORD': 'likelion1234',
-        'HOST': 'swulev.chkailloujbx.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
->>>>>>> 893b35e3e6cd993fc0e7d78bbfc914a9c6870c96
     }
     
 }
