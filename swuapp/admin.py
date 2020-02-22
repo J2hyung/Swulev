05b60d344aefdd6e91631a72f385a38b663cda99
babyJIMIN <duo2208@likelion.org>
2020-02-23 02:44:26 +0900

    admin에 lecture list 띄우기

--- swuapp/admin.py
+++ swuapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lecture
 
 # Register your models here.
+@admin.register(Lecture)
+class Lecture(admin.ModelAdmin):
+    list_display = ['lectureid', 'lecturename', 'professor', 'semester']
