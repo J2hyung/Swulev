6153c84fc04ea40d582ed27192f0c4e0af58d751
babylionNH <nancy1507@likelion.org>
2020-02-23 00:57:45 +0900

    model 끝

--- swuapp/models.py
+++ swuapp/models.py
@@ -1,22 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
 class User(models.Model):
-    userid = models.CharField(max_length = 20)
+    userid = models.CharField(max_length = 20,primary_key=True,unique=True, null=False)
     userpw = models.CharField(max_length = 30)
 
 class UserLecture(models.Model):
-
     RATING_FIELD = (
         ('on', 'on'),
         ('off', 'off'),
     )
-
-    myuserid = models.ForeignKey('User', on_delete=models.CASCADE, related_name='myuserid', unique=True,primary_key=True)
-    mylectureid = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name='mylectureid')
-    mysemester = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name='mysemester')
-    rating = models.CharField(max_length=10, choices=RATING_FIELD, default = "off")
-
+    l_userid = models.ForeignKey('User', on_delete=models.CASCADE, related_name='l_userid', unique=True,null=False,primary_key=True)
+    l_lectureid = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name='l_lectureid', unique=True)
 
 class Lecture(models.Model):
     semester = models.CharField(max_length = 30)
@@ -25,5 +20,13 @@ class Lecture(models.Model):
     professor = models.CharField(max_length=50)
 
 class Board(models.Model):
+    boardid = models.IntegerField(primary_key=True)
     text = models.CharField(max_length=100)
+    qualityscore = models.IntegerField()
+    difficultyscore = models.IntegerField()
+    recommendation = models.IntegerField()
+    declaration = models.IntegerField()
+    b_userid =  models.ForeignKey('User', on_delete=models.CASCADE, related_name='b_userid', unique=True)
+    b_lectureid = models.ForeignKey('Lecture', on_delete=models.CASCADE, related_name='b_lectureid', unique=True)
+
     
--- swuapp/views.py
+++ swuapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Lecture
-
+#from .models import Lecture
+from .models import User
 # Create your views here.
 
 def login(request):
-    user1 = Lecture.objects.raw('select * from swuapp_lecture')[0]
-    return render(request, 'login.html', {'user1':user1})
+    #user1 = Lecture.objects.raw('select * from swuapp_lecture')[0]
+    user_data = User(
+            userid = "dd" ,
+            userpw = "dd")
+    user_data.save()
+            # 새 객체 INSERT
+        
+    return render(request, 'login.html',{'user_data':user_data})
 
 def main(request):
     return render(request, 'main.html')
